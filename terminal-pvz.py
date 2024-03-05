58869637df5260cbf943aacd45281ca23724451c
Steven Little <stevenroblittle@gmail.com>
2024-03-05 14:49:40 +1300

    Improved Rendering to show Pea movement more clearly

--- terminal-pvz.py
+++ terminal-pvz.py
@@ -6,8 +6,8 @@ from random import randint
 from cursor_utils import hide_cursor, show_cursor
 
 CANVAS_HEIGHT = 5
-CANVAS_WIDTH = 10
-EMPTY_CHAR = "  "
+CANVAS_WIDTH = 30
+EMPTY_CHAR = " "
 
 STARTING_SUN = 50
 
@@ -20,37 +20,40 @@ def share_position(entity1, entity2):
     return [entity1.position[0] == entity2.position[0], entity1.position[1] == entity2.position[1]].count(True) == 2
 
 class Entity:
-    def __init__(self, health, representation, position):
+    def __init__(self, health, representation, position, width):
         self.health = health
         self.representation = representation
+        self.width = width
         self.position: list[int] = position
         self.tick_count = 0
 
 class Zombie(Entity):
     def __init__(self, position):
         self.representation = "🧟"
+        self.width = 2
         self.health = 100
-        super().__init__(self.health, self.representation, position)
+        super().__init__(self.health, self.representation, position, self.width)
     
     def tick(self):
         blocked = False
         self.tick_count += 1
         for entity in entities:
-            if isinstance(entity, Plant):
-                if entity.position[0] == self.position[0] - 1 and entity.position[1] == self.position[1]:
+            if isinstance(entity, Plant) or isinstance(entity, Zombie) and entity != self:
+                if entity.position[0] == self.position[0] - entity.width and entity.position[1] == self.position[1]:
                     blocked = True
-                    entity.health -= 10
-                    if entity.health <= 0:
-                        entities.remove(entity) 
+                    if isinstance(entity, Plant):
+                        entity.health -= 10
+                        if entity.health <= 0:
+                            entities.remove(entity) 
         if not blocked:
             if self.tick_count % 10 == 0:     
                 self.position[0] -= 1
         
 class Plant(Entity):
-    def __init__(self, health, representation, position):
+    def __init__(self, health, representation, position, width):
         self.health = health
         self.representation = representation
-        super().__init__(self.health, self.representation, position)
+        super().__init__(self.health, self.representation, position, width)
         
     def tick(self):
         pass
@@ -58,14 +61,16 @@ class Plant(Entity):
 class Sunflower(Plant):
     def __init__(self, position):
         self.representation = "🌻"
+        self.width = 2
         self.health = 100
-        super().__init__(self.health, self.representation, position)
+        super().__init__(self.health, self.representation, position, self.width)
         
 class Peashooter(Plant):
     def __init__(self, position):
         self.representation = "🌱"
+        self.width = 2
         self.health = 100
-        super().__init__(self.health, self.representation, position)
+        super().__init__(self.health, self.representation, position, self.width)
         
         self.pea_cooldown = 0
     
@@ -73,20 +78,28 @@ class Peashooter(Plant):
         if self.pea_cooldown > 0:
             self.pea_cooldown -= 1
         else:
-            entities.append(Pea([self.position[0] + 1, self.position[1]]))
-            self.pea_cooldown = 10
+            zombie_in_front = False
+            for entity in entities:
+                if isinstance(entity, Zombie):
+                    if entity.position[0] >= self.position[0] and entity.position[1] == self.position[1]:
+                        zombie_in_front = True
+            if zombie_in_front:
+                entities.append(Pea([self.position[0], self.position[1]]))
+                self.pea_cooldown = 10
         
 class Wallnut(Plant):
     def __init__(self, position):
         self.representation = "🌰"
+        self.width = 2
         self.health = 300
-        super().__init__(self.health, self.representation, position)
+        super().__init__(self.health, self.representation, position, self.width)
         
 class Pea(Entity):
     def __init__(self, position):
-        self.representation = "O "
+        self.representation = u"\u2022"
+        self.width = 1
         self.health = 1
-        super().__init__(self.health, self.representation, position)
+        super().__init__(self.health, self.representation, position, self.width)
         
     def tick(self):
         for entity in entities:
@@ -100,17 +113,19 @@ class Pea(Entity):
 class LAWNMOWER(Entity):
     def __init__(self, position):
         self.representation = "🚜"
+        self.width = 2
         self.health = 1
-        super().__init__(self.health, self.representation, position)
+        super().__init__(self.health, self.representation, position, self.width)
         self.mode = "idle"
         
     def tick(self):
         for entity in entities:
             if isinstance(entity, Zombie):
+                is_overlapping = entity.position[0] == self.position[0] + self.width and entity.position[1] == self.position[1]
                 if self.mode == "idle":
-                    if share_position(entity, self):
+                    if is_overlapping:
                         self.mode = "active"
-                if share_position(entity, self) and self.mode == "active":
+                if is_overlapping and self.mode == "active":
                     entity.health = 0
         if self.mode == "active":
             self.position[0] += 1
@@ -120,8 +135,14 @@ class Cursor:
         self.position = position
         
     def move(self, x, y):
-        self.position[0] += x
-        self.position[1] += y
+        if x > 0 and self.position[0] + x * 3 < CANVAS_WIDTH:
+            self.position[0] += x * 3
+        elif x < 0 and self.position[0] + x * 3 >= 0:
+            self.position[0] += x * 3
+        if y > 0 and self.position[1] + y < CANVAS_HEIGHT:
+            self.position[1] += y
+        elif y < 0 and self.position[1] + y >= 0:
+            self.position[1] += y
         
 cursor = Cursor([0, 0])
         
@@ -154,16 +175,23 @@ def update_canvas(canvas, entities):
     clear_dead_entities(entities)
     clear_out_of_bounds_entities(entities)
     
-    for entity in entities:
+    for entity in reversed(entities):
         if is_out_of_bounds(entity):
             entities.remove(entity)
             continue
         x, y = entity.position
-        canvas[y][x] = entity.representation
+        # update non-space characters with non-space characters
+        for i in range(entity.width):
+            # set chars to zero width space
+            canvas[y][x + i] = u"\u200B"
+        
+        for i, c in enumerate(entity.representation):
+            canvas[y][x + i] = c
         
     # draw cursor
     x, y = cursor.position
-    canvas[y][x] = " X"
+    canvas[y][x] = " "
+    canvas[y][x + 1] = "X"
     
 def reset_cursor_position(cursor_travel_count):
     if cursor_travel_count > 0:
@@ -197,8 +225,8 @@ def clear_out_of_bounds_entities(ents):
             ents.remove(entity)
 
 def spawn_zombie(game_tick_count):
-    if game_tick_count % 27 == randint(0, 20):
-        entities.append(Zombie([CANVAS_WIDTH - 1, randint(0, CANVAS_HEIGHT - 1)]))
+    if game_tick_count % 27 == randint(0, 27):
+        entities.append(Zombie([CANVAS_WIDTH - 2, randint(0, CANVAS_HEIGHT - 1)]))
 
 def main():
     hide_cursor()
@@ -208,20 +236,19 @@ def main():
     game_tick_count = 0
 
     with keyboard.Listener(on_press=on_press) as listener:
-        entities.append(Zombie([9, 2]))
-        entities.append(Sunflower([1, 2]))
-        entities.append(Wallnut([4, 2]))
-        
         for i in range(CANVAS_HEIGHT):
             entities.append(LAWNMOWER([0, i]))
         
         for i in range(CANVAS_HEIGHT):
-            entities.append(Sunflower([1, i]))
+            entities.append(Sunflower([3, i]))
             
         for i in range(CANVAS_HEIGHT):
-            entities.append(Peashooter([2, i]))
+            entities.append(Peashooter([6, i]))
+            
+        for i in range(CANVAS_HEIGHT):
+            entities.append(Wallnut([9, i]))
         
-        for _ in range(300):
+        for _ in range(10000):
             # game events
             spawn_zombie(game_tick_count)
             update_canvas(canvas, entities)
@@ -235,7 +262,7 @@ def main():
             cursor_travel_count += print_canvas(canvas)
             
             game_tick_count += 1
-            sleep(0.2)
+            sleep(0.17)
     
 if __name__ == "__main__":
     main()
